211dcb614db0881c7cf7b0db66c21ddd26be1fd0
Arishah Khan <arishahsanaullah78@gmail.com>
2025-02-08 23:27:21 +0500

    Working

--- src/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardPage() {
-    return (
-      <div>
-        <h1 className="text-2xl font-bold">🚀 Welcome to Admin Dashboard</h1>
-        <p>{`Manage your food truck’s orders, menu, and users here.`}</p>
-      </div>
-    );
-  }
-  
